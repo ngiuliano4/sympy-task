a15c95d22b83def14ac734acdf9526c441f0e307
nellogiuliano <n.giuliano4@studenti.unisa.it>
2026-05-25 16:55:53 +0200

    ho importato la funzione limit dalla libreria sympy e ho calcolato il valore limite attraverso la funzione limit()

--- task.py
+++ task.py
@@ -24,18 +24,21 @@ def calcola_limite(espressione: str, variabile: str, punto: str) -> sympy.Expr:
     pass
 
 def calcola_polinomio_taylor(espressione: str, variabile: str, punto: float, ordine: int) -> sympy.Expr:
-    """Sub-task 4: Calcolare una Serie di Taylor."""
+    from sympy import symbols, series
+    x = symbols('x')
+    expr = series(espressione, variabile,punto,ordine)
+    return expr
     pass
+d
+ef risolvi_sistema_lineare(eq1: str, eq2: str, var1: str, var2: str) -> Dict[sympy.Symbol, sympy.Expr]:
 
-def risolvi_sistema_lineare(eq1: str, eq2: str, var1: str, var2: str) -> Dict[sympy.Symbol, sympy.Expr]:
-    """Sub-task 5: Risolvere un Sistema Lineare."""
     pass
 
 def main():
     print("Sub-task 1:", calcola_derivata("4*x+5", "x"))
     print("Sub-task 2:", calcola_integrale_definito("4*x**2", "x", 0, 3))
     print("Sub-task 3:", calcola_limite("(g**2 - 1)/(g - 1)", "g", "1"))
-    print("Sub-task 4:", calcola_polinomio_taylor("exp(x)", "x", 0.0, 4))
+    print("Sub-task 4:", calcola_polinomio_taylor("cos(x)", "x", 0.0, 4))
     print("Sub-task 5:", risolvi_sistema_lineare("x + y - 3", "x - y - 1", "x", "y"))
 
 if __name__ == "__main__":
